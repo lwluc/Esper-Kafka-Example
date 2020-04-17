3de9acdec82eea96ac90b39b2c9ee121e92b45cd
Luc Weinbrecht <luc@weinbrecht.de>
2020-04-17 09:13:00 +0200

    initial commit

---
+++ src/main/java/de/luc/weinbrecht/esperKafkaExample/EsperKafkaExampleApplication.java
@@ -0,0 +1,13 @@
+package de.luc.weinbrecht.esperKafkaExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsperKafkaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsperKafkaExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/luc/weinbrecht/esperKafkaExample/EsperKafkaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.luc.weinbrecht.esperKafkaExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsperKafkaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
